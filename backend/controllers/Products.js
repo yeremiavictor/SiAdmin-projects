80e172e5a82a0248cf25bd0b5653dbdf29922b9e
yeremiavictor <yeremiavictor@gmail.com>
2022-12-07 12:58:28 +0700

    Add CRUD Products.js

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,21 +1,150 @@
 import Products from "../models/ProductModel.js";
+import Users from "../models/UserModel.js";
+import {Op} from "sequelize";
 
-export const getProducts = (req,res) => {
+export const getProducts = async (req,res) => {
+    try {
+        let response;
+        if(req.role === "admin"){
+            response = await Products.findAll({
+                attributes:['uuid', 'name','price'],
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            })
+            
+        }else{
+            response = await Products.findAll({
+                attributes:['uuid', 'name','price'],
+                where:{
+                    userId: req.userId
+                },
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            })
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 
 }
 
-export const getProductById = (req,res) => {
+export const getProductById = async (req,res) => {
+    try {
+        const product = await Products.findOne({
+            where:{
+                uuid:req.params.id
+            }
+        })
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"})
 
+        let response;
+        if(req.role === "admin"){
+            response = await Products.findOne({
+                attributes:['uuid', 'name','price'],
+                where:{
+                    id:product.id
+                },
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            })
+            
+        }else{
+            response = await Products.findOne({
+                attributes:['uuid', 'name','price'],
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                },
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            })
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
 
-export const createProduct = (req,res) => {
-
+export const createProduct = async (req,res) => {
+    const {name, price} = req.body;
+    try {
+        await Products.create({
+            name: name,
+            price: price,
+            userId: req.userId
+        })
+        res.status(201).json({msg: "Product berhasil dibuat"})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
 
-export const updateProduct = (req,res) => {
-
+export const updateProduct = async (req,res) => {
+    try {
+        const product = await Products.findOne({
+            where:{
+                uuid:req.params.id
+            }
+        })
+        
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"})
+        const {name, price} = req.body;
+        if(req.role === "admin"){
+           await Products.update({name,price},{
+            where: {
+                id:product.id
+            }
+           })
+            
+        }else{
+            if(req.userId !== product.userId) return res.status(403).json({msg: "Akses ditolak"})
+            await Products.update({name,price},{
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                }
+           })
+        }
+        res.status(200).json({msg: "Product berhasil diupdate"})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
 
-export const deleteProduct = (req,res) => {
-
+export const deleteProduct = async (req,res) => {
+    try {
+        const product = await Products.findOne({
+            where:{
+                uuid:req.params.id
+            }
+        })
+        
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"})
+        const {name, price} = req.body;
+        if(req.role === "admin"){
+           await Products.destroy({
+            where: {
+                id:product.id
+            }
+           })
+            
+        }else{
+            if(req.userId !== product.userId) return res.status(403).json({msg: "Akses ditolak"})
+            await Products.destroy({
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                }
+           })
+        }
+        res.status(200).json({msg: "Product berhasil di hapus"})
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }

088837ad3a2e419426fdfb9f306bdf5a5db2530d
yeremiavictor <yeremiavictor@gmail.com>
2022-12-07 09:46:53 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import session from "express-session"
 import dotenv from "dotenv";
+import UserRoute from './routes/UserRoute.js'
+import ProductRoute from './routes/ProductRoute.js'
+import db from './config/database.js'
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
+(async ()=>{
+    await db.sync()
+
+})();
 
 app.use(session({
     secret:process.env.SESS_SECRET,
@@ -22,6 +29,8 @@ app.use(cors({
 }));
 
 app.use(express.json())
+app.use(UserRoute)
+app.use(ProductRoute)
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('server up and running on port...')

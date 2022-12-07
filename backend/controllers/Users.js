69818bfb94a06868426f92b9b181cefb6f35ef7a
yeremiavictor <yeremiavictor@gmail.com>
2022-12-07 09:47:58 +0700

    init Controller product & users

---
+++ backend/controllers/Users.js
@@ -0,0 +1,21 @@
+import Users from "../models/UserModel.js"
+
+export const getUsers = (req,res) => {
+
+}
+
+export const getUserById = (req,res) => {
+
+}
+
+export const createUser = (req,res) => {
+
+}
+
+export const updateUser = (req,res) => {
+
+}
+
+export const deleteUser = (req,res) => {
+    
+}

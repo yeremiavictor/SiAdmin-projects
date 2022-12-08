41f56fdd31b0a8d0aca2819cd32478f91b3c00ec
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:25:24 +0700

    init pages ProductAdd & Product Edit

---
+++ frontend/src/pages/Product/ProductAdd.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layouts/Layout.js";
+import AddProduct from "../../components/Product/AddProduct.js";
+
+const ProductAdd = () => {
+  return (
+    <Layout>
+      <AddProduct />
+    </Layout>
+  );
+};
+
+export default ProductAdd;
---
+++ frontend/src/pages/Product/ProductEdit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layouts/Layout";
+import EditProduct from "../../components/Product/EditProduct";
+
+const ProductEdit = () => {
+  return (
+    <Layout>
+      <EditProduct />
+    </Layout>
+  );
+};
+
+export default ProductEdit;

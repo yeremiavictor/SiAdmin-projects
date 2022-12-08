f5a1451ce19b623370a65a7446ca24280d6eec09
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:18:19 +0700

    init App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.js";
+import Login from "./components/Auth/Login.js";
+import Users from "./pages/User/Users.js";
+import UserAdd from "./pages/User/UserAdd.js";
+import UserEdit from "./pages/User/UserEdit.js";
+import Products from "./pages/Product/Products.js";
 
 function App() {
   return (
-    <div >
-     Lorem ipsum dolor sit amet.
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* Users */}
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/add" element={<UserAdd />} />
+          <Route path="/users/edit/:id" element={<UserEdit />} />
+
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;

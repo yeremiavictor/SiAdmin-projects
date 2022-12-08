4fa23fd0ded9f6e6f1abe5fc7537a8c1ce5ef2c5
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:16:02 +0700

    init dashboard pages

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./Layouts/Layout.js";
+import Wellcome from "../components/Wellcome.js";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <Wellcome />
+    </Layout>
+  );
+};
+
+export default Dashboard;

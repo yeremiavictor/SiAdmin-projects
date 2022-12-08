70aebf143cb83730dd944edb7e7e554ae38de8d6
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:17:29 +0700

    init user crud components

---
+++ frontend/src/components/User/UserList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UserList = () => {
+  return (
+    <div>
+      <h1 className="title">Users</h1>
+      <h2 className="subtitle"> Daftar Pengguna</h2>
+      <table className="table is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Row</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserList;

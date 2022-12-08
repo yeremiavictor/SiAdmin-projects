9e58b586dbc98e97fedf7839275e4e6d7750177d
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:28:56 +0700

    Add Product Rooutes on APP js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Users from "./pages/User/Users.js";
 import UserAdd from "./pages/User/UserAdd.js";
 import UserEdit from "./pages/User/UserEdit.js";
 import Products from "./pages/Product/Products.js";
+import ProductAdd from "./pages/Product/ProductAdd.js";
+import ProductEdit from "./pages/Product/ProductEdit.js";
 
 function App() {
   return (
@@ -21,7 +23,10 @@ function App() {
           <Route path="/users/add" element={<UserAdd />} />
           <Route path="/users/edit/:id" element={<UserEdit />} />
 
+          {/* Products */}
           <Route path="/products" element={<Products />} />
+          <Route path="/products/add" element={<ProductAdd />} />
+          <Route path="/products/edit/:id" element={<ProductEdit />} />
         </Routes>
       </BrowserRouter>
     </div>

2a3d47e3406c601d3985f4922f1165b5d5f098d7
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:18:11 +0700

    init product component

---
+++ frontend/src/components/Product/ProductList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1 className="title">Product</h1>
+      <h2 className="subtitle"> Daftar Product</h2>
+      <table className="table is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Created By</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ frontend/src/pages/Product/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layouts/Layout";
+import ProductList from "../../components/Product/ProductList";
+
+const Products = () => {
+  return (
+    <Layout>
+      <ProductList />
+    </Layout>
+  );
+};
+
+export default Products;

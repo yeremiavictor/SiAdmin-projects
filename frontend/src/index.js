9911b77d413d5ff046bf767e1c3bd8b0ec93f195
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:45:34 +0700

    init axios on index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import 'bulma/css/bulma.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "bulma/css/bulma.css";
+import axios from "axios";
 
-const container = document.getElementById('root');
+// Agar credential terisinial pada setiap page
+axios.defaults.withCredentials = true;
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(

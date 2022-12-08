bc805f9345b35c6502359a6879cea7c685cd13e2
yeremiavictor <yeremiavictor@gmail.com>
2022-12-08 09:16:27 +0700

    init components wellcome

---
+++ frontend/src/components/Wellcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Wellcome = () => {
+  return (
+    <div>
+      <h1 className="title">Dashboard</h1>
+      <h2 className="subtitle">Wellcome back ... </h2>
+    </div>
+  );
+};
+
+export default Wellcome;

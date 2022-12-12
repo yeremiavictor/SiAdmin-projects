378477dc930c02381c48d59fcaefe6370f1cb434
yeremiavictor <yeremiavictor@gmail.com>
2022-12-12 10:58:42 +0700

    add user name on Wellcome.js

--- frontend/src/components/Wellcome.js
+++ frontend/src/components/Wellcome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Wellcome = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <h1 className="title">Dashboard</h1>
-      <h2 className="subtitle">Wellcome back ... </h2>
+      <h2 className="subtitle">Wellcome back ... {user && user.name}</h2>
     </div>
   );
 };

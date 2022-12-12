98399cd1afbf701b873f5d5c8c8ec42fd5505c1f
yeremiavictor <yeremiavictor@gmail.com>
2022-12-12 08:28:34 +0700

    ini store.js for authreducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authSlice";
+import authReducer from "../features/authSlice.js";
 
 export const store = configureStore({
   reducer: {

32b9f69b60e743eac4b502f1b9d9dcfa0bee38d0
yeremiavictor <yeremiavictor@gmail.com>
2022-12-12 11:15:56 +0700

    add axios fuction delete, add edit

--- frontend/src/components/Product/ProductList.js
+++ frontend/src/components/Product/ProductList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+  };
+
+  const deleteProduct = async (productId) => {
+    await axios.delete(`http://localhost:5000/products/${productId}`);
+    getProducts();
+  };
+
   return (
     <div>
       <h1 className="title">Product</h1>
       <h2 className="subtitle"> Daftar Product</h2>
+      <Link className="button is-primary mb-2" to="/products/add">
+        Add
+      </Link>
       <table className="table is-striped is-fullwidth">
         <thead>
           <tr>
@@ -16,13 +37,28 @@ const ProductList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {products.map((product, index) => (
+            <tr key={product.uuid}>
+              <td>{index + 1}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.user.name}</td>
+              <td>
+                <Link
+                  to={`/products/edit/${product.uuid}`}
+                  className="button is-small is-info"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => deleteProduct(product.uuid)}
+                  className="button is-small is-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
